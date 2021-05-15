d130a7332f514c7dfa7d1f27400e014d6e554340
Doha <doha.noaman.essa@gmail.com>
2021-05-15 08:26:15 +0200

    commited

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
-import 'package:date_time_format/date_time_format.dart';
+import 'package:jiffy/jiffy.dart';
+
+
 
 class MyCalendar extends StatefulWidget {
   @override
@@ -12,131 +14,129 @@ class MyCalendar extends StatefulWidget {
 
 class MyCalendarState extends State {
   CalendarController _controller;
-  DateTime myDate = DateTime.now();
-  bool visibleTF = false;
+
+DateTime myDate = DateTime.now();
+
+
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _controller = CalendarController();
   }
 
   void onDAySelected(DateTime date, List events, List holidays) {
     setState(() {
-      visibleTF = true;
+     // visibleTF = true;
       myDate = date;
+
+      showModalBottomSheet<void>(
+        context: context,
+          builder: (BuildContext context){
+          return Card(
+            color: Colors.white,
+            child: Column(
+              children: [
+                Text(
+                  "$myDate",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 27,
+                  ),
+                ),
+                Card(
+                  color: Colors.white60,
+                  child: Container(
+                    height: 100,
+                    width: 400,
+                    padding: EdgeInsets.fromLTRB(20.0, 10, 10, 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Haircut(2:40 PM)",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "It's getting too long ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white60,
+                  child: Container(
+                    height: 100,
+                    width: 400,
+                    padding: EdgeInsets.fromLTRB(20.0, 10, 10, 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Collect ransom(12:00 PM)",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "... or else ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+
+          },
+      );
+
+
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        TableCalendar(
-          initialCalendarFormat: CalendarFormat.month,
-          calendarStyle: CalendarStyle(
-              todayColor: Colors.orange,
-              todayStyle: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-              selectedColor: Theme
-                  .of(context)
-                  .primaryColor),
-          headerStyle: HeaderStyle(
-            formatButtonDecoration: BoxDecoration(
-              color: Colors.orange,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            formatButtonTextStyle: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
-            formatButtonShowsNext: false,
+    return TableCalendar(
+      initialCalendarFormat: CalendarFormat.month,
+      calendarStyle: CalendarStyle(
+          todayColor: Colors.orange,
+          todayStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
           ),
-          calendarController: _controller,
-          onDaySelected: onDAySelected,
+          selectedColor: Theme
+              .of(context)
+              .primaryColor),
+      headerStyle: HeaderStyle(
+        formatButtonDecoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(20),
         ),
-        Positioned(
-          top: 250,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Visibility(
-              visible: visibleTF,
-              child: Card(
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Text(
-                      "$myDate",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 27,
-                      ),
-                    ),
-                    Card(
-                      color: Colors.white60,
-                      child: Container(
-                        height: 100,
-                        width: 400,
-                        padding: EdgeInsets.fromLTRB(20.0, 10, 10, 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Haircut(2:40 PM)",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              "It's getting too long ",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Card(
-                      color: Colors.white60,
-                      child: Container(
-                        height: 100,
-                        width: 400,
-                        padding: EdgeInsets.fromLTRB(20.0, 10, 10, 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Collect ransom(12:00 PM)",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              "... or else ",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )),)
-      ],
+        formatButtonTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold),
+        formatButtonShowsNext: false,
+      ),
+      calendarController: _controller,
+      onDaySelected: onDAySelected,
     );
   }
 }
